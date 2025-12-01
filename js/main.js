2f5123e8fcc0019f9a19259f4ae1bee19f9af3a0
Тимофей <timofej.popov06@mail.ru>
2025-12-01 12:28:14 +0500

    Правда или действие

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import {generatePhotos} from './data.js';
 import {renderPhotos} from './mini-pictures.js';
-
+import { initFormValidation } from './form-validator.js';
 renderPhotos(generatePhotos());
+initFormValidation();
